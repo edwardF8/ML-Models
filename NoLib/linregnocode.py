960ec9e1e2149e8525695a1960c98bc630afed9d
youngthugfan7 <alifesaverme@gmail.com>
2023-11-05 09:57:02 -0500

    test

--- NoLib/linregnocode.py
+++ NoLib/linregnocode.py
@@ -8,17 +8,18 @@ with open('NoLib\medical_cost.csv','r') as file: #opens csv and saves under file
     for row in csv_reader:
         data.append(row)
 for row in data:
-    row[3] /= 53.1 #scaling BMI to values in between 0 and 1
-    row[7] /= 63770.42801 #scaling price, will multiply label by 63770.42801
-    row[4] /= 5.0
+    row[3] = float(row[3]) / 53.1 #scaling BMI to values in between 0 and 1
+    row[7] = float(row[5]) / 63770.42801 #scaling price, will multiply label by 63770.42801
+    row[4] = float(row[5]) /5.0
     if(row[2] == "female"):
-        row[2] = 1
+        row[2] = float(1)
     else:
-        row[2] = 0
+        row[2] = float(0)
     if(row[5] == 'yes'):
-        row[5] = 1
+        row[5] = float(1)
     else:
-        row[5] = 0
+        row[5] = float(0)
+        
 normalset = data[:1001]
 testset = data[1001:]
 
