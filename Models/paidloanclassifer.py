0ce1fa2660e266b8d32561169a97118c005574ab
youngthugfan7 <alifesaverme@gmail.com>
2024-01-09 17:23:24 -0500

    started paidloan classifier, added one hot encoding

---
+++ Models/paidloanclassifer.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('ML-Models\\data\\lending_club_loan_two.csv')
+#me figuring out what to drop and what to encode
+'''
+print("term unqiue: ",len(pd.unique(df['term'])))
+print("grade unqiue: ",len(pd.unique(df['grade'])))
+print("emp_length: ",len(pd.unique(df['emp_length'])))
+print("home_owndership: ",len(pd.unique(df['home_ownership'])))
+print("verification_status: ",len(pd.unique(df['verification_status'])))
+print("loan_stauts: ",len(pd.unique(df['loan_status'])))
+print("purpose: ",len(pd.unique(df['purpose'])))
+print("application_type: ",len(pd.unique(df['application_type'])))
+'''
+df = df.drop(columns=["sub_grade","emp_title","issue_d","title","earliest_cr_line","address","initial_list_status"])
+
+#uhh method i found online for easy one hot encoding
+def encode_and_bind(original_dataframe, feature_to_encode):
+    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+    res = pd.concat([original_dataframe, dummies], axis=1)
+    res = res.drop([feature_to_encode], axis=1)
+    return(res) 
+#
+features_to_encode = ['term','grade','emp_length','home_ownership','verification_status',"purpose","application_type"]
+for feature in features_to_encode:
+    df = encode_and_bind(df,feature)
+
+X = df.drop(columns=["loan_status"])
+#we act gonna convert all the ones that are fully paid to 1, and the other to 0
+Y = (df[['loan_status']] == 'Full Paid')
+print(Y.describe())
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.3)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler  = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+'''
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose =1, patience=25)
+model = Sequential([
+    Dense(58,activation='relu'),
+    Dropout(0.3),
+    Dense(25,activation='relu'),
+    Dropout(0.3),
+    Dense(10,activation='relu'),
+    Dropout(0.3),
+    Dense(1,activation='sigmoid')
+])
+'''
--- pracmodels/winequalityprediction.py
+++ pracmodels/winequalityprediction.py
@@ -4,7 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 #analyze data
-winedataset = pd.read_csv('data/winequality-white.csv', sep=";",names=["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","quality"])
+
+var = input("TrainingLaptop or PC: ")
+csv = ""
+if(var == "Laptop"):
+    csv = 'ML-Models\\data\\winequality-white.csv'
+elif(var == "PC"):
+    csv = 'data\\winequality-white.csv'
+
+
+winedataset = pd.read_csv(csv, sep=";",names=["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","quality"])
+
 winedataset.dropna(inplace=True)
 
 
@@ -36,7 +46,8 @@ normalizer.adapt(x_train)
 
 model = tf.keras.Sequential([
     tf.keras.layers.InputLayer(input_shape=(11,)),
-    tf.keras.layers.Dense(units=8,activation='tanh'),
+    tf.keras.layers.Dense(units=11,activation='tanh'),
+    tf.keras.layers.Dense(units=4,activation='tanh'),
     tf.keras.layers.Dense(units=1,activation='relu')
 ])
 
@@ -47,11 +58,13 @@ model = tf.keras.Sequential([
 #fit the model using our data
 print("Training Model on trainning data:")
 model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3), loss=tf.keras.metrics.mean_squared_error,metrics=[tf.keras.metrics.mse])
-history = model.fit(x_train,y_train,epochs=50, batch_size=16)
+model.fit(x_train,y_train,epochs=50, batch_size=16, validation_data=[x_test,y_test])
 
 #pass validation data in before badding validation_data=x,y in the .fit
 
-
+losses = pd.DataFrame(model.history.history)
+losses.plot()
+plt.show()
 
 print("\nEvaluating Model Preformance")
 predictions = model.evaluate(x_test,y_test,batch_size = 128)
