556e1b9ed4990bb8250183122741194fbed2e831
youngthugfan7 <alifesaverme@gmail.com>
2024-01-16 00:30:12 -0500

    sync

--- Models/irismodelnotfollow.py
+++
@@ -1,7 +0,0 @@
-import pandas
-
-name = ['Id','age','sex','bmi','children','smoker','region','charges']
-
-dt = pandas.read_csv('pracmodels\medcos.csv', names= name)
-
-dt.describe
--- Models/paidloanclassifer.py
+++ Models/paidloanclassifer.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-df = pd.read_csv('ML-Models\\data\\lending_club_loan_two.csv')
+df = pd.read_csv('data\\lending_club_loan_two.csv')
 #me figuring out what to drop and what to encode
 '''
 print("term unqiue: ",len(pd.unique(df['term'])))
@@ -31,7 +30,10 @@ for feature in features_to_encode:
 
 X = df.drop(columns=["loan_status"])
 #we act gonna convert all the ones that are fully paid to 1, and the other to 0
-Y = (df[['loan_status']] == 'Full Paid')
+Y = df[['loan_status']]
+Y.replace({'loan_status':{'Full Paid':1}},inplace=True)
+Y.replace({'loan_status':{'Charged Off':0}},inplace=True)
+Y.loc[]
 print(Y.describe())
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.3)
