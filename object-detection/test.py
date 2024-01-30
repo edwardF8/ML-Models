14499c4c328f85066704b1ed39552ab619a6661a
youngthugfan7 <alifesaverme@gmail.com>
2024-01-30 08:10:25 -0500

    Sync on Laptop

---
+++ object-detection/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+from tf.keras.models import Sequential
+from tf.keras.layers import Conv2D, Dense, Flatten, Dropout
+
+model = Sequential([
+    Conv2D(
+])
